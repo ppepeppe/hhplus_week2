0a3cbef1ec2a6c365237396994cb3abbb8eaacfe
byunseongdo <dmdsns@naver.com>
2024-12-26 17:58:43 +0900

    초기 세팅

---
+++ hhplus_week2_cleanArchitecture/src/main/java/org/example/hhplus_week2_cleanarchitecture/HhplusWeek2CleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package org.example.hhplus_week2_cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusWeek2CleanArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusWeek2CleanArchitectureApplication.class, args);
+    }
+
+}
---
+++ hhplus_week2_cleanArchitecture/src/test/java/org/example/hhplus_week2_cleanarchitecture/HhplusWeek2CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hhplus_week2_cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusWeek2CleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
